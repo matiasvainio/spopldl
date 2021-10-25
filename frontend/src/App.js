9f235c32d34733f579e70430a4a663d6bdfcb4b7
Matias Vainio <matias.s.vainio@gmail.com>
2021-10-25 22:00:52 +0300

    trying to dl all tracks in app component. wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 import { Login } from './components/Login';
 import { getTokenFromUrl } from './utils/spotify';
@@ -6,30 +6,43 @@ import { useStateContext } from './context/StateContext';
 import { Home } from './components/Home';
 import { setToken } from './utils/utils';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { getRecords } from './utils/utils';
 import './App.css';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [, dispatch] = useStateContext();
+  const [state, dispatch] = useStateContext();
+
+  // const getRecords = async (playlists) => {
+  //   const arr = [];
+  //   if (playlists.items) {
+  //     for (let i = 0; i < playlists.items.length; i++) {
+  //       const record = await spotify.getPlaylist(playlists.items[i].id, {
+  //         limit: 20,
+  //       });
+  //       arr.push(record.tracks.items);
+  //     }
+  //   }
+  //   console.log(arr);
+  //   return arr;
+  // };
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     // window.location.hash = '';
     const hashToken = hash.access_token;
 
-    const records = getRecords();
-
     const setCurrentUser = async (token) => {
       spotify.setAccessToken(token);
       const user = await spotify.getMe();
       const playlists = await spotify.getUserPlaylists({ limit: 50 });
+      // const records = await getRecords(playlists);
 
       setToken(token);
       dispatch({ type: 'SET_USER', user: user });
       dispatch({ type: 'SET_TOKEN', token: token });
       dispatch({ type: 'SET_PLAYLISTS', playlists: playlists });
+      // dispatch({ type: 'SET_RECORDS', records: records });
     };
 
     setCurrentUser(hashToken);
