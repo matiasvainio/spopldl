2f76fd50689da2e34e8f6b556a64c1a0efd390d6
Matias Vainio <matias.s.vainio@gmail.com>
2021-10-26 13:56:26 +0300

    custom colors added to config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: ["'Share Tech Mono'"],
+    },
+    extend: {
+      colors: {
+        background: '#303030',
+        sidebar: '#505050',
+        sidebarText: '#B5B5B5',
+        hover: '#E1E1E1',
+      },
+    },
   },
   variants: {
     extend: {},
